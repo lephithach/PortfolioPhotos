612934a6dee9d036ff88fc3f9f853903081b623d
Phi Thach <jokivadet@gmail.com>
2023-06-02 17:08:18 +0700

    first commit

---
+++ js/data.js
@@ -0,0 +1,53 @@
+export const data = [
+  {
+    url: "IMG_9049_Phi_Thach.jpg",
+    type: "",
+    date: "2023-06-01",
+  },
+  {
+    url: "IMG_9137_Phi_Thach.jpg",
+    type: "",
+    date: "2023-06-01",
+  },
+  {
+    url: "IMG_9152_Phi_Thach.jpg",
+    type: "",
+    date: "2023-06-01",
+  },
+  {
+    url: "IMG_9154_Phi_Thach.jpg",
+    type: "",
+    date: "2023-06-01",
+  },
+  {
+    url: "IMG_9252_Phi_Thach.jpg",
+    type: "",
+    date: "2023-06-01",
+  },
+  {
+    url: "IMG_9262_Phi_Thach.jpg",
+    type: "",
+    date: "2023-06-01",
+  },
+  {
+    url: "IMG_9286_Phi_Thach.jpg",
+    type: "",
+    date: "2023-06-01",
+  },
+];
+
+let html = "";
+// Render data
+data.forEach((item) => {
+  html += `
+        <img
+            src="./images/${item.url}"
+            alt="picture"
+            class="image"
+        />
+    `;
+});
+
+const galeryElement = document.querySelector(".galery");
+
+galeryElement.innerHTML = html;
---
+++ js/function.js
@@ -0,0 +1,26 @@
+const modalElement = document.querySelector(".modal");
+const modalImage = modalElement.querySelector("img");
+const galeryElement = document.querySelector(".galery");
+const galeryImage = galeryElement.querySelectorAll("img");
+
+// Show image modal
+galeryImage.forEach((image) => {
+  image.addEventListener("click", () => {
+    // Lock scroll
+    document.querySelector("body").style.overflow = "hidden";
+    // Show modal
+    modalElement.classList.add("show");
+
+    // Add url img
+    let getUrl = image.getAttribute("src");
+    modalImage.setAttribute("src", getUrl);
+  });
+});
+
+// Remove modal
+modalElement.addEventListener("click", () => {
+  modalElement.classList.remove("show");
+  document.querySelector("body").style.overflow = "";
+});
+
+async function render() {}
